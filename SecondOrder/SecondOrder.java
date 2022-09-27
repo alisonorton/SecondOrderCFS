fcd1a6445c8fefbcd3077196caa3de799ed3e25c
alisonorton <orton.alison@gmail.com>
2022-09-26 16:56:26 -1000

    Reading in data from keyboard

--- SecondOrder/SecondOrder.java
+++ SecondOrder/SecondOrder.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SecondOrder{
@@ -6,12 +7,26 @@ public class SecondOrder{
         //Scanner to read data
         var s = new Scanner(System.in);
         //since we know we're only reading in four numbers
-        double nums[] = new double[4];
+        int nums[] = new int[4];
 
         for(int i = 0; i < 4; i++){
-            nums[i] = s.nextDouble();
+            String input = s.nextLine();
+            nums[i] = Integer.parseInt(input.substring(input.length() -1));
+        }
+
+        System.out.println(Arrays.toString(nums));
+/* 
+        for(int i = 0; i < 4; i++){
+            nums[i] = s.nextInt();
         }
         s.close();
+
+        int s1 = nums[0];
+        int s2 = nums[1];
+        int c1 = nums[2];
+        int c2  = nums[3];
+
+        System.out.println("Scanned numbers" + nums);*/
 
     }
 }
