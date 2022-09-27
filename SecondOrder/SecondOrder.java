e0e63d177b6c8faafce857b29809065582fff00e
alisonorton <orton.alison@gmail.com>
2022-09-26 16:30:32 -1000

    storing input from scanner

--- SecondOrder/SecondOrder.java
+++ SecondOrder/SecondOrder.java
@@ -5,8 +5,13 @@ public class SecondOrder{
 
         //Scanner to read data
         var s = new Scanner(System.in);
-        int n = s.nextInt();
-        double nums[] = new double[n];
+        //since we know we're only reading in four numbers
+        double nums[] = new double[4];
+
+        for(int i = 0; i < 4; i++){
+            nums[i] = s.nextDouble();
+        }
+        s.close();
 
     }
 }
