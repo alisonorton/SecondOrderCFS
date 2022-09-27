e1d53bbb3e0697f6cd46f40e2fde8c24c09e6f6f
alisonorton <orton.alison@gmail.com>
2022-09-26 16:28:03 -1000

    Creating the scanner

--- SecondOrder/SecondOrder.java
+++ SecondOrder/SecondOrder.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class SecondOrder{
     public static void main(String[] args){
-        
+
+        //Scanner to read data
+        var s = new Scanner(System.in);
+        int n = s.nextInt();
+        double nums[] = new double[n];
+
     }
 }
