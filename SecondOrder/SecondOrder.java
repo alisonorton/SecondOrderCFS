36baa233c4e960460e20bb55441890430845c5ae
alisonorton <orton.alison@gmail.com>
2022-09-29 19:07:53 -1000

    Upadating comments

--- SecondOrder/SecondOrder.java
+++ SecondOrder/SecondOrder.java
@@ -7,9 +7,7 @@ public class SecondOrder{
 
         //Scanner to read data
         var s = new Scanner(System.in);
-        //since we know we're only reading in four numbers
-        //double nums[] = new double[4];
-
+        //Reading and storing data
         s.next();
         double s1 = s.nextDouble();
         s.next();
@@ -19,39 +17,29 @@ public class SecondOrder{
         s.next();
         double c2 = s.nextDouble() * (-1);
         s.close();
-
-        /* for(int i = 0; i < 4; i++){
-            String input = s.nextLine();
-            nums[i] = Double.parseDouble(input.substring(input.length() -1));
-        }
-        s.close(); */
-
-        //double s1 = nums[0];
-        //double s2 = nums[1];
-        //Our formula is t^2 - c1t - c2 = 0 so we multiply by -1
-        //double c1 = nums[2]*(-1);
-        //double c2  = nums[3]*(-1);
         double roots[] = new double[2];
 
-        //Find the roots
+        //Find and store the roots
         roots = findRoot(c1, c2);
         double r1 = roots[0];
         double r2 = roots[1];
+        //Display roots
         System.out.println("\nr1 = " + r1 + "\nr2 = " + r2);
 
-        //Finding p
+        //Find and display p
         double p = (s2 - s1*r2)/(r1 - r2);
         System.out.println("p = " + p);
 
-        //Finding q
+        //Find and display q
         double q = s1 - p;
         System.out.println("q = " + q);
 
         //Display the eqation
         System.out.println("S(n) = (" + p +")(" + r1 + ")^(n - 1) + (" + q + ")(" + r2 + ")^(n - 1)");
 
+        //Displaying S(1) and S(2)
         System.out.printf("S(1) = %.1f \nS(2) = %.1f\n", s1, s2);
-        //Calculating S(n)
+        //Calculate and display S(n)
         for(int i = 3; i <= 10; i++){
             double total = p * Math.pow(r1, i - 1) + q * Math.pow(r2, i -1);
             System.out.println("S(" + i + ") = " + total); 
