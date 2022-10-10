1e412b211c13c6a1b4278bdd2648fe5a877c54f5
alisonorton <orton.alison@gmail.com>
2022-10-10 13:44:06 -1000

    My first commit

--- SecondOrder/SecondOrder.java
+++ SecondOrder/SecondOrder.java
@@ -41,7 +41,7 @@ public class SecondOrder{
         System.out.printf("S(1) = %.1f \nS(2) = %.1f\n", s1, s2);
         //Calculate and display S(n)
         for(int i = 3; i <= 10; i++){
-            double total = p * Math.pow(r1, i - 1) + q * Math.pow(r2, i -1);
+            double total = p * Math.pow(r1, i - 1) + q * Math.pow(r2, i - 1);
             System.out.println("S(" + i + ") = " + total); 
         }
     
@@ -62,30 +62,10 @@ public class SecondOrder{
         
         double determinant = b * b - 4 * a * c;
 
-        // check if determinant is greater than 0
-        if (determinant > 0) {
-
         // two real and distinct roots
         root1 = (-b + Math.sqrt(determinant)) / (2 * a);
         root2 = (-b - Math.sqrt(determinant)) / (2 * a);
-      }
 
-      // check if determinant is equal to 0
-      else if (determinant == 0) {
-
-        // two real and equal roots
-        // determinant is equal to 0
-        // so -b + 0 == -b
-        root1 = root2 = -b / (2 * a);
-      }
-
-      // if determinant is less than zero
-      else {
-        // roots are complex number and distinct
-      double real = -b / (2 * a);
-      double imaginary = Math.sqrt(-determinant) / (2 * a);
-      return roots;
-      }
 
 
       roots[0] = root1;
